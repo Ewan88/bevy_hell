8e0741a7802f22d17867bfb822cf1afb9889b38c
@Ewan88 <baileyewan@gmail.com>
2023-07-10 00:23:10 +0100

    lerps camera

--- src/camera.rs
+++ src/camera.rs
@@ -10,7 +10,7 @@ pub struct CameraPlugin;
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, setup)
-            .add_systems(Update, move_camera);
+            .add_systems(PostUpdate, move_camera);
     }
 }
 
@@ -25,10 +25,9 @@ fn move_camera(
 ) {
     let Ok(player_transform) = player_query.get_single() else { return; };
     let Ok(mut camera_transform) = camera_query.get_single_mut() else { return; };
-    let diffx = player_transform.translation.x - camera_transform.translation.x;
-    let diffy = player_transform.translation.y - camera_transform.translation.y;
-    camera_transform.translation.x +=
-        diffx * time.delta_seconds() * input::MOVEMENT_SPEED;
-    camera_transform.translation.y +=
-        diffy * time.delta_seconds() * input::MOVEMENT_SPEED;
+
+    camera_transform.translation = camera_transform.translation.truncate().lerp(
+        player_transform.translation.truncate(),
+        0.1,
+    ).extend(999.);
 }
