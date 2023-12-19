936fae5a1fb2fae58bb05e5b2965349fe63805d2
Ewan <baileyewan@gmail.com>
2023-12-19 23:19:38 +0000

    health status

--- src/ui.rs
+++ src/ui.rs
@@ -3,20 +3,39 @@ use crate::player::*;
 use bevy::prelude::*;
 
 #[derive(Component)]
-pub struct UI;
+pub struct PlayerHealth;
 
 pub struct UIPlugin;
 
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(PostStartup, build_ui);
+        app.add_systems(PostStartup, build_ui)
+            .add_systems(Update, update_ui);
     }
 }
 
 fn build_ui(mut commands: Commands) {
     commands
-        .spawn((NodeBundle { ..default() }, UI))
+        .spawn(NodeBundle { ..default() })
         .with_children(|parent| {
-            parent.spawn(TextBundle::default());
+            parent.spawn((
+                TextBundle::from_section("", TextStyle::default()),
+                PlayerHealth,
+            ));
         });
+}
+
+fn update_ui(
+    player_query: Query<&Player>,
+    mut health_query: Query<&mut Text, With<PlayerHealth>>,
+) {
+    let Ok(player) = player_query.get_single() else {
+        return;
+    };
+
+    let Ok(mut text) = health_query.get_single_mut() else {
+        return;
+    };
+
+    text.sections[0].value = format!("Health: {}", player.health);
 }
